7befcd13922a13cfc5b0e8208ed110a58a3595fa
tgambin <tgambin@gmail.com>
2017-06-19 12:03:33 +0200

    Update run_CODEX.R

--- R/run_CODEX.R
+++ R/run_CODEX.R
@@ -55,8 +55,8 @@ bedFile <- file.path(args[13])
 
 library(CODEX)
 finalcall <- matrix(nrow=0,ncol=14)
-chrs <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, "X", "Y", 
-         "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22")
+chrs <- c(1:22, "X", "Y", paste0("chr",c(1:22, "X", "Y"))
+          
 for(chr in chrs) {
   exomtarg <- read.table(bedFile, sep = '\t')
   exomtarg <- exomtarg[exomtarg[,1] == chr,]
