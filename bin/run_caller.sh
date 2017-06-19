122384de632805e1b35b2b0ecb8892c03e368ee9
Wiktor Kuśmirek <wiktor93@onet.com.pl>
2017-06-19 14:49:56 +0200

    path to files removed from conf file

--- bin/run_caller.sh
+++ bin/run_caller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# ./run_caller.sh --caller=codex --conf-file=./../conf/caller.yaml --caller-path=./../R/run_CODEX.R
+# ./run_caller.sh --caller=codex --conf-file=./../conf/caller.yaml --caller-path=./../R/run_CODEX.R --coverage-file=/home/wiktor/CNV-opt/data/EXAMPLE_BAMS/coverage.tsv --bed-file=/home/wiktor/CNV-opt/data/EXAMPLE_BAMS/EXOME.bed --sample-names-file=/home/wiktor/CNV-opt/data/EXAMPLE_BAMS/sampname
 
 function parse_yaml {
    local prefix=$2
@@ -30,6 +30,15 @@ case $i in
     ;;
     --caller-path=*)
     CALLER_PATH="${i#*=}"
+    ;;
+    --coverage-file=*)
+    COVERAGE_FILE="${i#*=}"
+    ;;
+    --bed-file=*)
+    BED_FILE="${i#*=}"
+    ;;
+    --sample-names-file=*)
+    SAMPLE_NAMES_FILE="${i#*=}"
  ;;
     *)
             # unknown option
@@ -49,7 +58,7 @@ if [ $CALLER == "CODEX" ]
 then
   echo "CODEX caller..."
   # TODO zmienic na nazwane parametry
-  Rscript $CALLER_PATH $codex_mapp_thresh $codex_cov_thresh_from $codex_cov_thresh_to $codex_length_thresh_from $codex_length_thresh_to $codex_gc_thresh_from $codex_gc_thresh_to $codex_k_from $codex_k_to $codex_lmax $codex_cov_file $codex_sample_names_file $codex_bed_file
+  Rscript $CALLER_PATH $codex_mapp_thresh $codex_cov_thresh_from $codex_cov_thresh_to $codex_length_thresh_from $codex_length_thresh_to $codex_gc_thresh_from $codex_gc_thresh_to $codex_k_from $codex_k_to $codex_lmax $COVERAGE_FILE $SAMPLE_NAMES_FILE $BED_FILE
 elif [ $CALLER == "XHMM" ]
 then
   echo "XHMM caller..."
