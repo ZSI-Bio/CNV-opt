98b586ce767f8a90ffa685989704c9ab5f61206b
tgambin <tgambin@gmail.com>
2017-06-23 10:19:49 +0200

    Create SampleTest.scala

---
+++ src/test/scala/sample/SampleTest.scala
@@ -0,0 +1,15 @@
+package sample
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.scalatest.FunSuite
+
+/**
+  * Created by marek on 21/06/2017.
+  */
+class SampleTest extends FunSuite with SharedSparkContext
+{
+
+  test("Sample spark Unit test"){
+
+  }
+}
