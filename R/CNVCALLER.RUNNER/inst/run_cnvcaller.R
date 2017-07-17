9267fe552954ac7e8ce5124b012c7edc62ef04c4
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2017-07-17 14:35:12 +0200

    Added chr to increase parallelism level (#39)
    * Added chr to increase parallelism level
    
    * Change data source to PGSQL

--- R/CNVCALLER.RUNNER/inst/run_cnvcaller.R
+++ R/CNVCALLER.RUNNER/inst/run_cnvcaller.R
@@ -29,6 +29,7 @@ read_parameters <- function(tabName, id, conn){
   K_from <- parameters[1,'k_from']
   K_to <- parameters[1,'k_to']
   lmax <- parameters[1,'lmax']
+  chr <- parameters[1,'chr']
   return(list(caller=caller, 
               cov_table=cov_table, 
               mapp_thresh=mapp_thresh, 
@@ -40,7 +41,8 @@ read_parameters <- function(tabName, id, conn){
               gc_thresh_to=gc_thresh_to, 
               K_from=K_from, 
               K_to=K_to, 
-              lmax=lmax))
+              lmax=lmax,
+              chr=chr))
 }
 
 save_calls <- function(calls, conn){
@@ -53,11 +55,12 @@ save_calls <- function(calls, conn){
   }
 }
 
-read_coverage_table <- function(cov_table, conn){
+read_coverage_table <- function(cov_table, conn,chr){
   #query <- paste("select * from ", cov_table, sep="")
-  query <- paste("select * from ", cov_table, sep="")
+  query <- paste("select sample_name,target_id,chr,pos_min,pos_max,cov_avg from ", cov_table," where chr='",chr,"'", sep="")
+  print(query)
   ds <- dbGetQuery(conn, query)
-  colnames(ds) <- c("sample_name", "target_id", "chr", "pos_min", "pos_max", "cov_avg")
+  colnames(ds) <- c("sample_name", "target_id", "chr", "pos_min", "pos_max", "cov_avg") #hardcoded column order!!!
   ds
 }
 
@@ -93,8 +96,8 @@ drv_psql <- JDBC("org.postgresql.Driver", "./postgresql-42.1.1.jar",identifier.q
 conn_psql <- dbConnect(drv_psql, "jdbc:postgresql://cdh00.ii.pw.edu.pl:15432/cnv-opt", "cnv-opt", "zsibio321")
 
 parameters <- read_parameters(opt$tabName, opt$id, conn_psql)
-#print(parameters)
-cov_table <- read_coverage_table(parameters$cov_table, conn_hive)
+print(parameters)
+cov_table <- read_coverage_table(parameters$cov_table, conn_psql,parameters$chr)
 #print(cov_table)
 calls <- run_caller(parameters, cov_table)
 #print(calls)
