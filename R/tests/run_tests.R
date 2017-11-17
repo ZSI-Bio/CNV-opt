fb4472c215d7d1ae8e0193c5bd69cc7f74761671
Wiktor Kuśmirek <kusmirekwiktor@gmail.com>
2017-11-17 19:23:36 +0100

    reinstall custom packages before test them

--- R/tests/run_tests.R
+++ R/tests/run_tests.R
@@ -4,12 +4,18 @@ if (length(which(installed.packages()[,1] == "CODEX")) == 0){
     source("http://bioconductor.org/biocLite.R")
     biocLite("CODEX")
 }
+if (length(which(installed.packages()[,1] == "ExomeDepth")) == 0){install.packages("ExomeDepth",repos="https://cloud.r-project.org/")}
+
+if (length(which(installed.packages()[,1] == "CODEXCOV")) > 0){remove.packages("CODEXCOV")}
+if (length(which(installed.packages()[,1] == "EXOMEDEPTHCOV")) > 0){remove.packages("EXOMEDEPTHCOV")}
+if (length(which(installed.packages()[,1] == "CNVCALLER.RUNNER")) > 0){remove.packages("CNVCALLER.RUNNER")}
+if (length(which(installed.packages()[,1] == "CNVCALLER.EVALUATOR")) > 0){remove.packages("CNVCALLER.EVALUATOR")}
 
 setwd('tests/')
 devtools::install('../CODEXCOV')
+devtools::install('../EXOMEDEPTHCOV')
 devtools::install('../CNVCALLER.RUNNER')
 devtools::install('../CNVCALLER.EVALUATOR')
-
 
 library(testthat)
 out <- capture.output(test_dir(".", reporter="junit"))
