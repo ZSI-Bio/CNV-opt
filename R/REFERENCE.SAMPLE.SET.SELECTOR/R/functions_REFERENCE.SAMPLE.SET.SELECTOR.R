c10cd45b63a17815dee6c378b62d8032fae476d2
Wiktor Kuśmirek <kusmirekwiktor@gmail.com>
2017-12-18 00:04:53 +0100

    function for selecting reference sample set from ExomeDepth cnv caller

--- R/REFERENCE.SAMPLE.SET.SELECTOR/R/functions_REFERENCE.SAMPLE.SET.SELECTOR.R
+++ R/REFERENCE.SAMPLE.SET.SELECTOR/R/functions_REFERENCE.SAMPLE.SET.SELECTOR.R
@@ -1,4 +1,4 @@
-library(CODEX)
+library(ExomeDepth)
 
 coverageObj1 <- function(cov_table, sampname, targets){
   Y <- matrix(data=as.integer(0), nrow = nrow(targets), ncol = 0)
@@ -19,5 +19,20 @@ canoes_method <- function(investigated_sample, Y, num_refs){
   covariances <- cov[investigated_sample, reference_samples]
   reference_samples <- names(sort(covariances, 
           decreasing=T)[1:min(num_refs, length(covariances))])
+  return(list(reference_samples=reference_samples))
+}
+
+exomedepth_method <- function(investigated_sample, Y, num_refs, target_length){
+  samples <- colnames(Y)
+  reference_samples <- setdiff(samples, investigated_sample)
+  reference_set <- select.reference.set(test.counts = Y[,investigated_sample],
+                                        reference.counts = Y[,reference_samples],
+                                        bin.length = target_length,
+                                        n.bins.reduced = 10000)
+  #reference_samples <- reference_set$reference.choice
+  reference <- reference_set$summary.stats[1:num_refs,'ref.samples']
+  reference_samples <- c()
+  for (s in reference)
+    reference_samples <- c(reference_samples, c(s)) 
   return(list(reference_samples=reference_samples))
 }
