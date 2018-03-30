200a8f678f506df1302e3eadc3f47baa7a3a369f
Wiktor Kuśmirek <kusmirekwiktor@gmail.com>
2018-03-30 13:38:41 +0200

    another test of rebuilding docker image

--- build.sh
+++ build.sh
@@ -34,7 +34,11 @@ do
   diffTs=`echo "$(date +%s) - $(git log -n 1 --pretty=format:%at ${dir})" | bc`
   if [ $diffTs -lt $MAX_COMMIT_TS_DIFF ]; then
     cd $dir
-    docker rmi biodatageeks/cnv-opt-target-qc
+    if [[ ${image} == "biodatageeks/cnv-opt-target-qc" ]]; then
+      echo "Rebuilf of ${image} image forced..."
+      docker build --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t $image:$version .
+      docker build --build-arg CACHE_DATE=$(date +%Y-%m-%d:%H:%M:%S) -t $image:latest .
+    fi
     docker build --no-cache -t $image:$version .
     docker build --no-cache -t $image:latest .
     if [[ ${BUILD_MODE} != "local" ]]; then
