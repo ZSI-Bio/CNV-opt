0497387921e95c786fa96912299b58d09b35ee2c
Wiktor Kuśmirek <kusmirekwiktor@gmail.com>
2018-10-17 16:31:04 +0200

    first version of X replacement from Ximmer tool

--- R/CNV.SIMULATOR/R/run_CNV.SIMULATOR.R
+++ R/CNV.SIMULATOR/R/run_CNV.SIMULATOR.R
@@ -13,8 +13,8 @@ run_CNV.SIMULATOR <- function(input_cov_table,
   Y <- read.csv(input_cov_table)
   sampname <- colnames(Y)
   targets <- read.delim(input_bed)
-  males <- read.csv(input_males)
-  females <- read.csv(input_females)
+  males <- as.character(unlist(read.table(input_males, sep = ",")))
+  females <- as.character(unlist(read.table(input_females, sep = ",")))
   generated_cnvs <- matrix(nrow=0, ncol=4)
   colnames(generated_cnvs) <- c('sample','chr','st_bp','ed_bp')
   if (simulation_mode == "downsample") {
@@ -32,9 +32,26 @@ run_CNV.SIMULATOR <- function(input_cov_table,
       }
     }
   } else if (simulation_mode == "replace") {
-  # TODO
+    Y_males <- Y[,males]
+    Y_females <- Y[,females]
+    for (female in females) {
+      print(paste("Generating arficial CNVs in sample: ", female, sep=""))
+      male <- males[floor(runif(1, min=1, max=length(males)))]
+      for (i in 1:number_of_cnvs_per_sample) {
+        cnv_length <- floor(runif(1, min=min_number_of_regions, max=max_number_of_regions))
+        cnv_start <- floor(runif(1, min=1, max=nrow(targets)))
+        for (j in cnv_start:(min(cnv_start+cnv_length-1,nrow(targets)))) {
+          Y_females[j,female] <- Y_males[j,male]
+          Y[j,female] <- Y[j,male]
+        }
+        print(paste(female, targets[cnv_start,1], targets[cnv_start,2], targets[cnv_start+cnv_length,3], sep=" "))
+        generated_cnvs <- rbind(generated_cnvs, matrix(c(female, targets[cnv_start,1], targets[cnv_start,2], targets[cnv_start+cnv_length,3]), nrow = 1))
+      }
+    }
+    write.csv(Y_males, paste(output_cov_table, ".males", sep=""), row.names=F, quote=F)
+    write.csv(Y_females, paste(output_cov_table, ".females", sep=""), row.names=F, quote=F)
   } else {
-  # TODO
+    print("Choose proper simulation mode!!!")
   }
   write.csv(Y, output_cov_table, row.names=F, quote=F)
   write.csv(generated_cnvs, output_generated_cnvs, row.names=F, quote=F)
